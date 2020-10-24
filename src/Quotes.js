c555a30d7a33569f8cd49014aa3aaa1d7d78b27f
ajisegiri morenikeji <ajisegirimorenikeji@gmail.com>
2020-10-24 13:42:34 -0700

    update index.css

--- src/Quotes.js
+++ src/Quotes.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
-import './Quotes.css'
 
 
 class Quotes extends Component {
    render(){
   const generateRandomColor = this.props.displayColor();
-  const pageBackground = document.documentElement;
-  pageBackground.style.color = generateRandomColor;
+  const quoteBackground = document.documentElement;
+  quoteBackground.style.color = generateRandomColor;
    
   return(
-    <div className='quote-container' >
-      <div className='main-container'>
+    <div className='container' >
+      <div className='quote-container'>
       <div className='quote-box' key={Math.random()} style={{ backgroundColor: generateRandomColor}}>
-          <h1 className="quote" style={{ color: "white" }}>"{this.props.quote}"</h1>
-          <h5 className="author">-{this.props.author ? this.props.author : "Unknown"}-</h5>
+          <h1 className="quote" style={{ color: "white" }}>{this.props.quote}</h1>
+          <h5 className="author">{this.props.author ? this.props.author : "Unknown"}</h5>
       </div>
-      <button style={{ backgroundColor: generateRandomColor }} className="newquote"onClick={this.props.handleClick}>
-        New quote
-      </button>
+       <div className='cta'>
+       <button style={{ backgroundColor: generateRandomColor }} className="newquote" onClick={this.props.handleClick}>
+        Newquote
+       </button>
       </div>
+     </div>
     </div>
      )
    }
